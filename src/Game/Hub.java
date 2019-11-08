99bbfaf0175f914c58adff1c4ee20cb3437f7570
malinsommar <malinsommar1997@gmail.com>
2019-11-08 14:29:45 +0100

    Initial commit.

--- src/Game/Hub.java
+++ src/Game/Hub.java
@@ -1,16 +1,53 @@
 package Game;
 
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.util.Scanner;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Hub {
 
     Scanner sc = new Scanner(System.in);
+    JFrame frame = new JFrame();
+
+
 
     public Hub(){
 
-        mainMenu();
+        frameMenu();
     }
 
+
+    //Jframe
+    public void frameMenu(){
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = (JPanel) frame.getContentPane(); //ett paket man kan lägga in flera lable/Buttons i för att sortera dem.
+        panel.setLayout(null);
+
+        JLabel gameName = new JLabel("*SPELETS TITEL*");
+        JButton newRunButton = new JButton("New Run");
+        JButton tutorialButton = new JButton("Tutorial");
+        JButton exitButton = new JButton("Exit game");
+
+        panel.add(gameName);
+
+        Dimension size = gameName.getPreferredSize();
+        gameName.setBounds(240, 10, size.width, size.height);  //sätter vart Lable ska vara någonstans. Men fungerar inte när set.layout är med.
+
+        panel.add(newRunButton);
+        frame.add(tutorialButton);
+        frame.add(exitButton);
+
+        frame.setLayout(new FlowLayout()); //Deafult layout
+        frame.setSize(600, 300);
+        frame.setVisible(true);
+    }
+
+    //Ren Java
     public void mainMenu(){
         //New run, tutorial, exit
 
