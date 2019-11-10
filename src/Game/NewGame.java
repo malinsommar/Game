f1088bf43e400e444d011627b199bdba8a89cfa1
Davidfurby <57434673+Davidfurby@users.noreply.github.com>
2019-11-10 16:09:47 +0100

    Dampat runt och gjort blåa linjer som rör sig ¯\_(ツ)_/¯

--- src/Game/NewGame.java
+++ src/Game/NewGame.java
@@ -13,11 +13,11 @@ public class NewGame{
 
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        ImageIcon picmain = new ImageIcon("C:\\Users\\96simben\\Documents\\GitHub\\Game\\src\\Game\\tavern.jpg"); //laddar tavern bild
-        ImageIcon picforest = new ImageIcon("C:\\Users\\96simben\\Documents\\GitHub\\Game\\src\\Game\\forest.jpg");//laddar forest bild
-        ImageIcon piccave = new ImageIcon("C:\\Users\\96simben\\Documents\\GitHub\\Game\\src\\Game\\cave.jpg");//laddar cave bild
-        //String picmenu = ("C:\\Users\\96simben\\Documents\\GitHub\\Game\\src\\Game\\tavern.jpg"); hehe pathen är en string hehe
-        gameFrame.setContentPane(new JLabel(picmain)); //sätter backgrundsbild
+        ImageIcon picMain = new ImageIcon("tavern.jpg"); //download tavern image
+        ImageIcon picForest = new ImageIcon("forest.jpg");//download cavern image
+        ImageIcon picCave = new ImageIcon("cave.jpg");//download cave image
+        //String picMenu = ("C:\\Users\\96simben\\Documents\\GitHub\\Game\\src\\Game\\tavern.jpg"); hehe pathen är en string hehe
+        gameFrame.setContentPane(new JLabel(picMain)); //assign background
         /*
         till senare (placerar text på botten
         studentsButton.setHorizontalTextPosition(AbstractButton.CENTER);
@@ -31,8 +31,8 @@ public class NewGame{
         gameFrame.setLayout(new FlowLayout()); //Default layout
         gameFrame.setSize(1920, 1080);
         gameFrame.setVisible(true);
-        forestButton = new JButton(picforest); //
-        caveButton = new JButton(piccave); //
+        forestButton = new JButton(picForest); //
+        caveButton = new JButton(picCave); //
         gameFrame.add(forestButton); //
         gameFrame.add(caveButton); //
 
