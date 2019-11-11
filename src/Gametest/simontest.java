f03c37faf6f26f80ad8a37e199b1e476c9ca5e08
Simon Kid Bengtsson <96simben@mk.se>
2019-11-11 15:04:47 +0100

    shop
    idklol

--- src/Gametest/simontest.java
+++ src/Gametest/simontest.java
@@ -3,15 +3,15 @@ package Gametest;
 import javax.swing.*;
 import java.awt.*;
 
-public class simontest extends JFrame {
+public class simontest extends JFrame{
 
     JFrame testFrame = new JFrame();
 
     public simontest(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        ImageIcon picCave = new ImageIcon("shopshop.jpg");//download cave image
-        setContentPane(new JLabel(picCave)); //assign background
+        ImageIcon picShop = new ImageIcon("shopshop.jpg");//download cave image
+        setContentPane(new JLabel(picShop)); //assign background
 
         setLayout(new FlowLayout()); //Default layout
         setSize(1920, 1080);
