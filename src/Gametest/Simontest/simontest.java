2ecc4f599973b213ebc60eb1975d14679d2483db
Simon Kid Bengtsson <96simben@mk.se>
2019-11-13 15:07:11 +0100

    musik i simontest

--- src/Gametest/Simontest/simontest.java
+++ src/Gametest/Simontest/simontest.java
@@ -3,12 +3,15 @@ package Gametest.Simontest;
 import javafx.scene.canvas.GraphicsContext;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.RescaleOp;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class simontest extends JFrame{
 
@@ -31,6 +34,23 @@ public class simontest extends JFrame{
         add(new JLabel(ranger));
         add(new JLabel(mage));
         add(new JLabel(healer));
+
+
+
+        try {
+            File yourFile = new File("C:\\Users\\96simben\\Documents\\GitHub\\Game\\src\\Gametest\\Simontest\\theme.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(yourFile);
+            Clip clip = (Clip) AudioSystem.getLine(new DataLine.Info(Clip.class, audioIn.getFormat()));
+            clip.open(audioIn);
+            FloatControl gainControl =
+                    (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-30.0f); //ändrar volym
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
+        catch (Exception e) {
+                //whatevers
+        }
 
 
         //BufferedImage mage2 = ImageIO.read(new File("mage.gif"));//funkar inte
@@ -58,7 +78,8 @@ public class simontest extends JFrame{
         setVisible(true);
 
     }
-        public static void main (String[]args) throws IOException {
+
+    public static void main (String[]args) throws IOException {
 
         new simontest();
 
