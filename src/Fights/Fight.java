6a36db47495d61b176168d30a01069719f611bba
Simon Kid Bengtsson <96simben@mk.se>
2019-11-14 11:16:51 +0100

    idk

--- src/Fights/Fight.java
+++ src/Fights/Fight.java
@@ -1,11 +1,8 @@
 package Fights;
 
-import Game.Warrior;
-
 public class Fight {
 
     public int turnOrder = 0;
-    Warrior warrior=new Warrior();
 
     public void fightStart(){
         if (turnOrder == 0){
