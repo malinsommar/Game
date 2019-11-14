3f8c27ee21cfc377c0f997485f4f402828ce39c5
Simon Kid Bengtsson <96simben@mk.se>
2019-11-14 11:30:03 +0100

    humanity restored

---
+++ Game-2590239de7337560f4dd4483c8be1dab23ff6fec/src/Weapons/HealerStartWeapon.java
@@ -0,0 +1,11 @@
+package Weapons;
+
+public class HealerStartWeapon {
+
+    public HealerStartWeapon(){
+        String name = "Wodden staff";
+        int damage = 5;
+        String rarety = "common";
+        System.out.println("Healer got a "+name+". Damage: " + damage+". Raraty: "+rarety);
+    }
+}
