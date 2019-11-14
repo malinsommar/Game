c5d0b606b2fa7bdd8041f74d129f98c2fa27e24f
Simon Kid Bengtsson <96simben@mk.se>
2019-11-14 11:00:20 +0100

    fight class start

---
+++ src/Fights/Fight.java
@@ -0,0 +1,20 @@
+package Fights;
+
+import Game.Warrior;
+
+public class Fight {
+
+    public int turnOrder = 0;
+    Warrior warrior=new Warrior();
+
+    public void fightStart(){
+        if (turnOrder == 0){
+
+
+        }
+
+
+
+
+    }
+}
