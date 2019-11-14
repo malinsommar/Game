338dd13df7e13045b4c8e134a8d6322133363773
Simon Kid Bengtsson <96simben@mk.se>
2019-11-14 12:28:50 +0100

    fightoptions

--- src/Game/SetStats.java
+++ src/Game/SetStats.java
@@ -1,13 +1,13 @@
 package Game;
 
+import java.util.ArrayList;
+
 public class SetStats {
 
     Warrior warrior = new Warrior();
     Mage mage = new Mage();
     Healer healer = new Healer();
     Ranger ranger = new Ranger();
-
-
 
 
     public void setStartPartyStat(){
