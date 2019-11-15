cc82d7d999a297b5ca2651a0dd4fd73cacd4332c
Simon Kid Bengtsson <96simben@mk.se>
2019-11-15 09:05:21 +0100

    firestorm idk

--- src/Fights/fightOption.java
+++ src/Fights/fightOption.java
@@ -3,18 +3,14 @@ package Fights;
 import java.lang.reflect.Array;
 
 public class fightOption {
-    public int turnOrder = 0;
 
     public void skills(Array enemies, String spell, int level){
 
-        if (spell == "firestorm"){
+        if (spell.equals("firestorm")){
             for (int i = 0; i < 4 ; i++) {
                 ForrestFight.enemies[i] -= (5 * level);
 
             }
         }
-
-
-
     }
 }
