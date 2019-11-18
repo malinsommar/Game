1ca8fe0c3abf6d4674a49c4253245672c88d506e
theoddboy <simoncraft3@hotmail.com>
2019-11-18 09:22:11 +0100

    skills start

--- src/Gametest/Simontest/simontest.java
+++ src/Gametest/Simontest/simontest.java
@@ -1,5 +1,6 @@
 package Gametest.Simontest;
 
+import Game.Warrior;
 import Game.musicpick;
 import javafx.scene.canvas.GraphicsContext;
 
@@ -13,13 +14,18 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
+import java.util.Scanner;
 
-public class simontest extends JFrame{
+public class simontest extends JFrame {
 
+    Scanner sc = new Scanner(System.in);
     JFrame testFrame = new JFrame();
     private JButton testButton;
+    private JButton testButton2;
+    int[] enemies = new int[4];
+    int level = 5;
 
-    public simontest() throws IOException {
+    public simontest() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
@@ -37,16 +43,90 @@ public class simontest extends JFrame{
         add(new JLabel(mage));
         add(new JLabel(healer));
 
-        testButton = new JButton("Tutorial");
+        testButton = new JButton("firestorm");
         testButton.setSize(300, 100);
-        testButton.setLocation(500, 330);
+        testButton.setLocation(200, 330);
         testButton.setBackground(Color.white);
 
+        testButton2 = new JButton("fireblast");
+        testButton2.setSize(300, 100);
+        testButton2.setLocation(100, 330);
+        testButton2.setBackground(Color.white);
+
         add(testButton);
+        add(testButton2);
+        testButton.addActionListener(e -> spelltest("firestorm", level));
+        testButton2.addActionListener(e -> spelltest("fireblast", level));
 
-        testButton.addActionListener(e -> musicpick.musicStart("themefull"));
+        //testButton.addActionListener(e -> musicpick.musicStart("theme"));
 
-        musicpick.musicStart("themefull");
+        //musicpick.musicStart("theme");
+
+
+        //////////////////////////////////////////////////////////////////
+
+        setLayout(new FlowLayout()); //Default layout
+        setSize(1900, 500);
+
+        setVisible(true);
+        sethp();
+    }
+
+    public void sethp() {
+        enemies[0] = 100;
+        enemies[1] = 100;
+        enemies[2] = 100;
+        enemies[3] = 100;
+    }
+
+    public void spelltest(String spell, int level) {
+        //int health = Warrior.hp;
+        //dmg = weapon damage + buffdmg + debuffdmg
+        //mage
+        if (spell.equals("firestorm")) {
+            for (int i = 0; i < 4; i++) {
+                enemies[i] -= (5 * level);
+                System.out.println("enemy nr " + i + " has " + enemies[i]);
+            }
+        } else if (spell.equals("fireball")) {
+            enemies[0] -= (10 * level);
+            System.out.println("enemy nr " + 0 + " has " + enemies[0]);
+        } else if (spell.equals("pyroblast")) {
+            enemies[0] -= (20 * level);
+            System.out.println("enemy nr " + 0 + " has " + enemies[0]);
+        } else if (spell.equals("arcane explosion")) {
+            for (int i = 0; i < 4; i++) {
+                enemies[i] -= (5 * level);
+                System.out.println("enemy nr " + i + " has " + enemies[i]);
+            }
+
+        }
+        //warrior
+        else if (spell.equals("concussive blow")) {
+            enemies[0] -= (3 * level);
+            //stunned 0 true
+            System.out.println("stunned the enemy, skipping it's next turn");
+            System.out.println("enemy nr " + 0 + " has " + enemies[0]);
+        }
+        else if (spell.equals("battlecry")) {
+            //buffdmg = 5
+            System.out.println("shouts loudly, inspireing their allies");
+        }
+        else if (spell.equals("demoralizing shout")) {
+            //debuffdmg = 5
+            System.out.println("shouts loudly, striking fear in the hearths of their enemies");
+        }
+        else if (spell.equals("taunt")) {
+            //taunted = true
+            System.out.println("insults the enemy, it is sure to attack them");
+        }
+        //ranger
+        else if (spell.equals("arrow shotshot blast")) {
+            enemies[0] -= (20 * level);
+            System.out.println("enemy nr " + 0 + " has " + enemies[0]);
+        }
+        //healer
+    }
 
 
 /*
@@ -86,13 +166,6 @@ public class simontest extends JFrame{
         warrior.setBounds(100, 100, warriorsize.width, warriorsize.height);
          */
 
-
-        setLayout(new FlowLayout()); //Default layout
-        setSize(1920, 1080);
-
-        setVisible(true);
-
-    }
 
     public static void main (String[]args) throws IOException {
 
